6939e462fa0db7fccd50749f9e4d0ea68abca7d5
superior44 <superioribori89@gmail.com>
2018-03-29 15:50:48 +0100

    Added html files

---
+++ icalculator.js
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title>icalculator</title>
+</head>
+<body bgcolor="B8860B">
+<center><br>
+<h1><b><font color ="white" style = "font-size:75"> javascript calculator </font><h1></B>
+<hr size=21 color ="black">
+<diy style= "width:261px; background:"FFF8DC">
+<from name ="calculator">
+<input name ="display" placeholder = "0" style ="width:254px; height:60; text-align: font-size:30; border-radius:8px; margin:3px"/>
+<br>
+<input type ="button" value="7" onClick ="document.calculator.display.value +='7'" style ="width60px; font-:30; border-radius:8px; margin:3px"/>
+<input type ="button" value="8" onClick ="document.calculator.display.value +='8'" style ="width60px; font-:30; border-radius:8px"/> 
+<input type ="button" value="9" onClick ="document.calculator.display.value +='9'" style ="width60px; font-:30; border-radius:8px"/>
+<input type ="button" value="+" onClick ="btnplus()" style ="width:60px; font-size:30; border-radius:8px"/>
+<br> 
+<input type ="button" value="4" onClick ="document.calculator.display.value +='7'" style ="width60px; font-:30; border-radius:8px; margin:3px"/>
+<input type ="button" value="5" onClick ="document.calculator.display.value +='8'" style ="width60px; font-:30; border-radius:8px"/> 
+<input type ="button" value="6" onClick ="document.calculator.display.value +='9'" style ="width60px; font-:30; border-radius:8px"/>
+<input type ="button" value="+" onClick ="btnplus()" style ="width:60px; font-size:30; border-radius:8px"/>
+<br>
+<input type ="button" value="1" onClick ="document.calculator.display.value +='7'" style ="width60px; font-:30; border-radius:8px; margin:3px"/>
+<input type ="button" value="2" onClick ="document.calculator.display.value +='8'" style ="width60px; font-:30; border-radius:8px"/> 
+<input type ="button" value="3" onClick ="document.calculator.display.value +='9'" style ="width60px; font-:30; border-radius:8px"/>
+<input type ="button" value="*" onClick ="btnplus()" style ="width:60px; font-size:30; border-radius:8px"/>
+<br>
+<input type ="button" value="0" onClick ="document.calculator.display.value +='7'" style ="width60px; font-:30; border-radius:8px; margin:3px"/>
+<input type ="button" value="%" onClick ="document.calculator.display.value +='8'" style ="width60px; font-:30; border-radius:8px"/> 
+<input type ="button" value="." onClick ="document.calculator.display.value +='9'" style ="width60px; font-:30; border-radius:8px"/>
+<input type ="button" value="/" onClick ="btnplus()" style ="width:60px; font-size:30; border-radius:8px"/>
+<br>
